63bda201447b1e36fb0ee263205c19f11f7d0a03
Angelika Brown <angelikpbrown@gmail.com>
2025-07-30 23:31:42 -0400

    upload function

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+@app.route('/upload', methods=['POST'])
+def upload():
+    return
 
 if __name__ == '__main__':
     app.run(debug=True)
