c7bd43013a90ad13b441014861345c9b429cd83a
Angelika Brown <angelikpbrown@gmail.com>
2025-07-30 23:41:17 -0400

    upload function

--- app.py
+++ app.py
@@ -17,9 +17,28 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/upload', methods=['POST'])
 def upload():
-    return
+    #grab the uploaded file
+    file = request.files['file']
+    # check if the file is a CSV
+    if not file or not file.filename.endswith('.csv'):
+        return "Please upload a valid CSV file.", 400
+    # read the CSV file into a DataFrame
+    df = pd.read_csv(file)
+
+    # give a summary of the DataFrame
+    summary = summarize_data(df)
+
+    #suggest cleaning to the user
+    cleaning = suggest_cleaning(df)
+
+
+    return f"""
+    
+        <h2>Here is your Summary</h2>
+    """
 
 if __name__ == '__main__':
     app.run(debug=True)
